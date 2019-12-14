627f60c50250465ce2191a67398e0079eaf4ee07
unknown <Admin@IHR80PBF171.continental.edu.pe>
2019-12-14 03:46:44 -0500

    escoger fecha y hora; detalle especialidad

---
+++ app/src/main/java/com/example/centromedico/ElegrHoraActivity.java
@@ -0,0 +1,14 @@
+package com.example.centromedico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ElegrHoraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_elegr_hora);
+    }
+}
---
+++ app/src/main/java/com/example/centromedico/EscojerFechaActivity.java
@@ -0,0 +1,14 @@
+package com.example.centromedico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EscojerFechaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escojer_fecha);
+    }
+}
