4f2f8cd1919805fbe6652c5b7e871aa94a591d74
unknown <Admin@IHR80PBF201.continental.edu.pe>
2019-12-18 19:52:50 -0500

    uniendo lo del developer 1

---
+++ app/src/main/java/com/example/centromedico/detalleespecialidadActivity.java
@@ -0,0 +1,14 @@
+package com.example.centromedico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detalleespecialidadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalleespecialidad);
+    }
+}
---
+++ app/src/main/java/com/example/centromedico/elegirhoraActivity.java
@@ -0,0 +1,14 @@
+package com.example.centromedico;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class elegirhoraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_elegirhora);
+    }
+}
